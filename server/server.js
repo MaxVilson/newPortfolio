d9ccc70326632fd5b9e1ac93a035b90217ba6a38
Max <maxim00066@gmail.com>
2021-02-06 19:39:02 +0300

    add static routes

--- server/server.js
+++ server/server.js
@@ -1,36 +1,41 @@
-const express = require('express')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv').config()
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const app = express();
 
-let port = process.env.PORT || 7777
+let port = process.env.PORT || 7777;
 
-dotenv.config()
+dotenv.config();
 
-mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true }, err => {
+mongoose.connect(
+  process.env.DATABASE,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
     if (err) {
-        console.log(err)
+      console.log(err);
     } else {
-        console.log('Connected to database')
+      console.log("Connected to database");
     }
-})
+  }
+);
 
 // Middlewares
-app.use(morgan('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(cors())
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.static("../client/dist"));
 
 // require apis
-const postRoutes = require('./routes/post')
-const portfolioRoutes = require('./routes/portfolio')
+const postRoutes = require("./routes/post");
+const portfolioRoutes = require("./routes/portfolio");
 
-app.use('/api', postRoutes)
-app.use('/api', portfolioRoutes)
+app.use("/api", postRoutes);
+app.use("/api", portfolioRoutes);
 
 app.listen(port, () => {
-    console.log(`server is started on ${port}`)
-})
+  console.log(`server is started on ${port}`);
+});
